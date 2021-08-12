25dac29b9a2b4eb14f2c79d4f5139f2035b445d5
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-12 23:47:38 +0000

    added the bean of passowrdencoder to the securityconfiguer

--- src/main/java/com/nana/jwtdemo/SecurityConfigurer.java
+++ src/main/java/com/nana/jwtdemo/SecurityConfigurer.java
@@ -3,6 +3,7 @@ package com.nana.jwtdemo;
 
 import com.nana.jwtdemo.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +24,11 @@ public class SecurityConfigurer  extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder().getInstance();
     }
 
 //    @Override
