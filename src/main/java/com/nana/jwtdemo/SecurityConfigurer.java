aa4a8e46c41e7ed30a658d9690ac9c19c3bd8c3e
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-12 23:37:17 +0000

    updating pom

--- src/main/java/com/nana/jwtdemo/SecurityConfigurer.java
+++ src/main/java/com/nana/jwtdemo/SecurityConfigurer.java
@@ -1,12 +1,31 @@
 package com.nana.jwtdemo;
 
 
+import com.nana.jwtdemo.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigurer  extends WebSecurityConfigurer{
+public class SecurityConfigurer  extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+//    @Override
+//    public void configure(webSecurity web) throws Exception{
+//
+//
+//    }
 }
