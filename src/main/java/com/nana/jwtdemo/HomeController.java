76db986991144e96d59b9bd80b5334cccf85f640
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-14 23:58:43 +0000

    addin thr jwt request '

--- src/main/java/com/nana/jwtdemo/HomeController.java
+++ src/main/java/com/nana/jwtdemo/HomeController.java
@@ -1,8 +1,10 @@
 package com.nana.jwtdemo;
 
+import com.nana.jwtdemo.models.JwtRequest;
 import com.nana.jwtdemo.models.JwtResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +15,7 @@ public class HomeController {
     }
 
     @PostMapping("/authenticate")
-    public JwtResponse authenticate
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest){
+
+    }
 }
