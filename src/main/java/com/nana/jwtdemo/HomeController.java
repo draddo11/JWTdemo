99ca9e4d40319edbb1bb0ff5e25a13d2d5f77bdb
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-14 23:53:14 +0000

    crating the homecontroller

---
+++ src/main/java/com/nana/jwtdemo/HomeController.java
@@ -0,0 +1,17 @@
+package com.nana.jwtdemo;
+
+import com.nana.jwtdemo.models.JwtResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to JWT Tutorial";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate
+}
