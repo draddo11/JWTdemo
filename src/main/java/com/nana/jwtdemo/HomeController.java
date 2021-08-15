e4bb60226280e9974959be905caf5693b7608fdd
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-15 23:43:15 +0000

    addig utility to the homecontroller

--- src/main/java/com/nana/jwtdemo/HomeController.java
+++ src/main/java/com/nana/jwtdemo/HomeController.java
@@ -2,6 +2,9 @@ package com.nana.jwtdemo;
 
 import com.nana.jwtdemo.models.JwtRequest;
 import com.nana.jwtdemo.models.JwtResponse;
+import com.nana.jwtdemo.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+     private JwtUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+
     @GetMapping("/")
     public String home(){
         return "Welcome to JWT Tutorial";
@@ -16,6 +31,7 @@ public class HomeController {
 
     @PostMapping("/authenticate")
     public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest){
+        authenticationManager.authenticate()
 
     }
 }
