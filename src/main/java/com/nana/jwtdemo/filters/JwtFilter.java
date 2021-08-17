2ffd44b445e9602cc3f6547743c51fffa3f6cbe3
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-17 22:27:34 +0000

    addinthe logioc for checking auth in filter

--- src/main/java/com/nana/jwtdemo/filters/JwtFilter.java
+++ src/main/java/com/nana/jwtdemo/filters/JwtFilter.java
@@ -1,5 +1,9 @@
 package com.nana.jwtdemo.filters;
 
+import com.nana.jwtdemo.service.MyUserDetailsService;
+import com.nana.jwtdemo.utility.JwtUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,9 +12,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@Component
 public class JwtFilter  extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtility jwtUtility;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+
+
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+        String username = null;
+                String jwtToken = null ;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            jwtToken = authorizationHeader.substring(7);
+            username = jwtUtility.getUsernameFromToken(jwtToken);
+
+        }
 
     }
 }
